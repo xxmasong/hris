a88d4a9ab145e0307c38581eecfd885f7a542934
Zen Masong <133077023+xxmasong@users.noreply.github.com>
2023-05-26 10:46:55 +0800

    Added Authentication in Routes

--- server/app/Http/Tables/RoutesTable.php
+++ server/app/Http/Tables/RoutesTable.php
@@ -4,14 +4,16 @@ namespace App\Http\Tables;
 
 use App\Http\Props\RouteProps;
 use App\Http\Props\Routes\AuthenticationRouteProps;
-use App\Http\Props\Routes\AccountRouteProps;
 use App\Http\Props\Routes\CommonRouteProps;
-use App\Http\Props\Routes\NewsRouteProps;
 
 class RoutesTable extends RouteProps
 {
     public const ROUTE_TABLE = [
-        self::ROUTE_API => [],
+        self::ROUTE_API => [
+            AuthenticationRouteProps::REGISTER,
+            AuthenticationRouteProps::LOGIN,
+            AuthenticationRouteProps::LOGOUT,
+        ],
         self::ROUTE_WEB => [
             CommonRouteProps::VIEW_TELESCOPE,
             CommonRouteProps::VIEW_MODELS,
