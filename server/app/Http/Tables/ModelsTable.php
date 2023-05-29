d5b46eea8114e3eae7e333e04168d3402af3b4b1
Zen Masong <133077023+xxmasong@users.noreply.github.com>
2023-05-29 23:32:15 +0800

    Update all tables

--- server/app/Http/Tables/ModelsTable.php
+++ server/app/Http/Tables/ModelsTable.php
@@ -3,14 +3,36 @@
 namespace App\Http\Tables;
 
 use App\Http\Props\ModelProps;
+use App\Models\Article;
+use App\Models\ArticleCategory;
+use App\Models\ArticleCountry;
+use App\Models\Category;
+use App\Models\Country;
+use App\Models\User;
+use App\Models\UserCategory;
+use App\Models\UserCountry;
 
 class ModelsTable extends ModelProps
 {
     public const MODEL_TABLE = [
+        Category::MODEL,
+        Country::MODEL,
+        User::MODEL,
+        UserCategory::MODEL,
+        UserCountry::MODEL,
     ];
 
     public const MIGRATIONS_UP = self::MODEL_TABLE;
     public const MIGRATIONS_DOWN = self::MODEL_TABLE;
 
-    public const SEEDERS = [];
+    public const SEEDERS = [
+        // Tables with Factories
+        Category::MODEL,
+        Country::MODEL,
+        // Tables with ...
+        User::MODEL,
+        // Tables with ...
+        UserCategory::MODEL,
+        UserCountry::MODEL,
+    ];
 }
--- server/app/Http/Tables/RoutesTable.php
+++ server/app/Http/Tables/RoutesTable.php
@@ -4,7 +4,9 @@ namespace App\Http\Tables;
 
 use App\Http\Props\RouteProps;
 use App\Http\Props\Routes\AuthenticationRouteProps;
+use App\Http\Props\Routes\AccountRouteProps;
 use App\Http\Props\Routes\CommonRouteProps;
+use App\Http\Props\Routes\NewsRouteProps;
 
 class RoutesTable extends RouteProps
 {
@@ -13,6 +15,16 @@ class RoutesTable extends RouteProps
             AuthenticationRouteProps::REGISTER,
             AuthenticationRouteProps::LOGIN,
             AuthenticationRouteProps::LOGOUT,
+            AccountRouteProps::CURRENT_USER,
+            AccountRouteProps::CURRENT_COUNTRY,
+            AccountRouteProps::CURRENT_CATEGORIES,
+            NewsRouteProps::ALL_COUNTRIES,
+            NewsRouteProps::ALL_CATEGORIES,
+            NewsRouteProps::ARTICLES,
+            NewsRouteProps::SOURCES,
+            AccountRouteProps::UPDATE_USER,
+            AccountRouteProps::UPDATE_COUNTRY,
+            AccountRouteProps::UPDATE_CATEGORY,
         ],
         self::ROUTE_WEB => [
             CommonRouteProps::VIEW_TELESCOPE,
